bba6dfb73174b97c37fb14b94236116e84c48902
mdradityatama <mdimasradityatama@gmail.com>
2019-12-24 22:59:48 +0700

    penambahan seed db

--- Startup.cs
+++ Startup.cs
@@ -15,20 +15,33 @@ namespace MvcMovie
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
 
-            services.AddDbContext<MvcMovieContext>(options => 
-                    options.UseSqlite(Configuration.GetConnectionString("MvcMovieContext")));
+            if (Environment.IsDevelopment())
+            {
+                services.AddDbContext<MvcMovieContext>(options => 
+                options.UseSqlite(
+                    Configuration.GetConnectionString("MvcMovieContext")));
+            } 
+            else 
+            {
+                services.AddDbContext<MvcMovieContext>(options => 
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("MvcMovieContext")));
+
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,7 +68,7 @@ namespace MvcMovie
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Movies}/{action=Index}/{id?}");
             });
         }
     }
